a4bbec200f1a1438e7a21d3b625e8bf33cbe3ed0
Cynthia Shao <cynthia_shao@berkeley.edu>
2024-10-19 21:50:07 -0700

    Added comments to the remaining taskInput files.

--- frontend/calendar-gen-todo/src/app/taskInput/generateButton.jsx
+++ frontend/calendar-gen-todo/src/app/taskInput/generateButton.jsx
@@ -1,5 +1,5 @@
-import { Button } from '@mantine/core' // Importing Button component from Mantine.
+import { Button } from '@mantine/core' // Import Button component from Mantine.
 
-export default function GenerateButton() {
+export default function GenerateButton() { // Renders an outlined button component.
   return <Button type="button" variant="outline" className="ml-2">Generate</Button>
 }
